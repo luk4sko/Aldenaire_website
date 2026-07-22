6f21872abe9d7cfbfe4e2fa1e301808b7585f068
Aldenaire <lukasbrtko1@gmail.com>
2026-07-22 18:53:53 +0200

    Remove duplicate nested rocnikovy_projekt folder

--- rocnikovy_projekt/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // odstráni všetky premenné session
-session_destroy(); // zničí session
-header("Location: home.php"); // presmerovanie na home page
-exit();
-?>
