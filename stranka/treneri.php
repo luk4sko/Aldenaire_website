4183f881b3f8322561adeca1eeafdd23f3ad0eb7
Aldenaire <lukasbrtko1@gmail.com>
2026-07-22 19:26:25 +0200

    Adjust profile dropdown: higher, shifted right, z-index above everything

--- stranka/treneri.php
+++ stranka/treneri.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tréneri</title>
-    <link rel="stylesheet" href="style.css?v=2">
+    <link rel="stylesheet" href="style.css?v=3">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body class="treneri_page">
