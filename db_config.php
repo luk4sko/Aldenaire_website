70259886794fe50672bd5dab255cd1560b1b7742
Aldenaire <lukasbrtko1@gmail.com>
2026-07-22 18:50:43 +0200

    Add .gitignore and remove db_config.php from version control

--- db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$pdo = new PDO(
-    "pgsql:host=aws-0-eu-west-3.pooler.supabase.com;port=5432;dbname=postgres",
-    "postgres.tzmwequninfkosgfxiee",
-    "Gdtfjuu55ege."
-);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
